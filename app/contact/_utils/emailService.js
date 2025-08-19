4c2419af933b5d341f2609e12ebe3912f6d1e4ca
Ahmed Omran <ahmedomran1950@gmail.com>
2025-08-19 07:03:57 +0300

    edit & test emailjs

--- app/contact/_utils/emailService.js
+++ app/contact/_utils/emailService.js
@@ -1,10 +1,9 @@
 export const sendEmailService = async (emailjs, formData, projectType) => {
-  const serviceId = import.meta.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-  const projectTemplateId = import.meta.env
-    .NEXT_PUBLIC_EMAILJS_PROJECT_TEMPLATE_ID;
-  const collabTemplateId = import.meta.env
-    .NEXT_PUBLIC_EMAILJS_COLLABORATION_TEMPLATE_ID;
-  const publicKey = import.meta.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+  const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+  const projectTemplateId = process.env.NEXT_PUBLIC_EMAILJS_PROJECT_TEMPLATE_ID;
+  const collabTemplateId =
+    process.env.NEXT_PUBLIC_EMAILJS_COLLABORATION_TEMPLATE_ID;
+  const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
 
   if (!serviceId || !projectTemplateId || !collabTemplateId || !publicKey) {
     throw new Error("Missing EmailJS environment variables");
