206f8ff5ac4066ec78ea97a2ea230d24552db2b4
2ao1 <2ao1.space@gmail.com>
2025-12-01 06:38:24 +0200

    edit resume

--- app/resume/_components/SkillsSection.js
+++ app/resume/_components/SkillsSection.js
@@ -65,23 +65,26 @@ export default function SkillsSection() {
           <div className="w-3/4">
             <h3 className="text-sm md:text-2xl font-bold mb-8">Projects</h3>
             {info.projects.map((project, index) => (
-              <div
-                key={index}
-                className="flex justify-between items-start py-2 border-b border-primary-900 gap-8"
-              >
-                <h5 className="text-sm md:text-base text-main">
-                  <AnimatedLink
-                    key={index}
-                    href={`/projects/${project.title
-                      .toLowerCase()
-                      .replace(" ", "-")}`}
-                  >
-                    {project.title}
-                  </AnimatedLink>
-                </h5>
-                <div className="hidden md:flex w-1/4 justify-end">
-                  <span>[ {project.date} ]</span>
+              <div key={index} className="mb-6">
+                <div
+                  key={index}
+                  className="flex justify-between items-start py-2 border-b border-primary-900 gap-8 "
+                >
+                  <h5 className="text-sm md:text-base text-main">
+                    <AnimatedLink
+                      key={index}
+                      href={`/projects/${project.title
+                        .toLowerCase()
+                        .replace(" ", "-")}`}
+                    >
+                      {project.title}
+                    </AnimatedLink>
+                  </h5>
+                  <div className="hidden md:flex w-1/4 justify-end">
+                    <span>[ {project.date} ]</span>
+                  </div>
                 </div>
+                <p>{project.bio}</p>
               </div>
             ))}
           </div>
